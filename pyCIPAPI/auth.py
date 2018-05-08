a09e03686789997d96a0a27343ebe2282197d558
sboardman <simon.boardman@gmail.com>
2018-05-08 13:07:29 +0100

    AuthenticatedOpenCGASEssion class added for working with openCGA

--- pyCIPAPI/auth.py
+++ pyCIPAPI/auth.py
@@ -1,6 +1,7 @@
 """Objects for authenticating with the GEL CIP API."""
 
 from __future__ import print_function, absolute_import
+import json
 import requests
 import maya
 from .auth_credentials import auth_credentials
@@ -39,6 +40,58 @@ class AuthenticatedCIPAPISession(requests.Session):
                         cip_auth_url, data=(auth_credentials))
                      .json()['token'])
             self.headers.update({"Authorization": "JWT " + token})
+            self.auth_time = maya.now()
+            self.auth_expires = self.auth_time.add(minutes=30)
+        except KeyError:
+            self.auth_time = False
+            print('Authentication Error')
+        return self
+
+    def check_auth(self):
+        """Check whether the session is still authenticated."""
+        if maya.now() > self.auth_expires():
+            self.authenticate()
+        else:
+            pass
+
+
+class AuthenticatedOpenCGASession(requests.Session):
+    """Subclass of requests Session for accessing GEL openCGA instance."""
+
+    def __init__(self):
+        """Init AuthenticatedOpenCGASession and run authenticate function.
+
+        Authentication credentials are stored in auth_credentials.py and are in
+        dictionary format:
+
+        auth_credentials = {"username": "username", "password": "password"}
+
+        """
+        requests.Session.__init__(self)
+        self.authenticate()
+
+    def authenticate(self):
+        """Use auth_credentials to generate an authenticated session.
+
+        Uses the cip_auth_url hard coded in and credentials in the
+        auth_credentials file to retrieve an authentication token from the CIP
+        API.
+
+        Returns:
+            The current instance of AuthenticatedOpenCGASession with the sid
+            value as an attribute, plus the auth_time and auth_expires time.
+        """
+        opencga_auth_url = ('https://apps.genomicsengland.nhs.uk/opencga/'
+                            'webservices/rest/v1/users/{username}/login'
+                            .format(username=auth_credentials['username']))
+        try:
+            self.headers.update({"Accept": "application/json",
+                                 "Content-Type": "application/json",
+                                 "Authorization": "Bearer "})
+            sid_response = (self.post(opencga_auth_url,
+                                      data=json.dumps(auth_credentials))
+                            .json())
+            self.sid = sid_response['response'][0]['result'][0]['sessionId']
             self.auth_time = maya.now()
             self.auth_expires = self.auth_time.add(minutes=30)
         except KeyError:
