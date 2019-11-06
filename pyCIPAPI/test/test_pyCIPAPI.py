d7102eef6ccb39b556cbd0373ceaa6f0af594107
Nana Mensah <Nana.mensah1@nhs.net>
2019-11-06 16:06:31 +0000

    Add tests for ir download and v6 validator

--- pyCIPAPI/test/test_pyCIPAPI.py
+++ pyCIPAPI/test/test_pyCIPAPI.py
@@ -11,7 +11,6 @@ import jellypy.pyCIPAPI.config as config
 import jellypy.pyCIPAPI.auth as auth
 import jellypy.pyCIPAPI.interpretation_requests as irs
 
-
 def test_import():
     """pyCIPAPI modules can be imported. Config string is readable from module."""
     assert bool(config.live_100k_data_base_url)
@@ -37,12 +36,21 @@ def test_auth(cipapi_session):
 class TestIRTools():
     def test_get_irjson(self, cipapi_session):
         """Interpretation request jsons can be downloaded from the CIPAPI"""
-        data = irs.get_ir_json(2202,2,cipapi_session,{'reports_v6':True})
-        assert 'interpreted_genome' in data.keys()
+        data = irs.get_ir_json(2202,2,cipapi_session)
+        assert isinstance(data, dict)
     
-    def test_v6_model(self, cipapi_session):
-        """Returned json is GeL v6 model"""
-        is_v6 = irs.get_ir_json(2202,2,cipapi_session,{'reports_v6':True})
-        not_v6 = irs.get_ir_json(2202,2,cipapi_session,{'reports_v6':True})
+    def test_v6_validator(self, cipapi_session):
+        """Returned json is GeL v6 model.
+        Although we use the reports_v6=True request parameter, earlier interpretation request jsons
+        are returned without v6 data structure. Here we test two examples with the gelmodels validation."""
+        is_v6 = irs.get_ir_json(54715,1,cipapi_session)
+        not_v6 = irs.get_ir_json(45,2,cipapi_session)
 
-        pass
+        v6_validator = irs.get_v6_interpreted_genome_validator()
+
+        # Test the first interpreted genome against the interpreted genome v6 model
+        is_v6_ig = is_v6['interpreted_genome'][0]['interpreted_genome_data']
+        not_v6_ig = not_v6['interpreted_genome'][0]['interpreted_genome_data']
+        
+        assert v6_validator.validate(is_v6_ig) == True
+        assert v6_validator.validate(not_v6_ig) == False
