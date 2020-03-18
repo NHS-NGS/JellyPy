824af0027c359f681a792104420e2f75b13aaa95
Andy Bond <20341309+andyb3@users.noreply.github.com>
2020-03-18 16:57:16 +0000

    Allow AD to be toggled on/off in config file

--- pyCIPAPI/jellypy/pyCIPAPI/example_auth_credentials.py
+++ pyCIPAPI/jellypy/pyCIPAPI/example_auth_credentials.py
@@ -1,6 +1,15 @@
 """Authentication credentials. NOT in Version Control."""
 
+
+"""
+NOTE:
+username and password refer to legacy LDAP username and password. These will be deprecated at AD switchover.
+"""
 auth_credentials = {
+    'username': "USERNAME",
+    'password': "PASSWORD",
     'client_id': "YOUR_CLIENT_ID",
-    'client_secret': "YOUR_CLIENT_SECRET"
+    'client_secret': "YOUR_CLIENT_SECRET",
+    'beta_client_id': "YOUR_BETA_CLIENT_ID",
+    'beta_client_secret': "YOUR_BETA_CLIENT_SECRET",
 }
