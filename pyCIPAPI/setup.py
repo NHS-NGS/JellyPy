28f70ce27272d83d98aa864d089cea71a1b7b292
Nana Mensah <Nana.mensah1@nhs.net>
2020-03-25 12:09:03 +0000

    Fix incorrect merge by removing tierup-lib code from pyCIPAPI

--- pyCIPAPI/setup.py
+++ pyCIPAPI/setup.py
@@ -1,7 +1,4 @@
-import pathlib
 from setuptools import find_packages, setup
-
-README = ( pathlib.Path(__file__).parent / 'readme.md').read_text()
 
 setup(
     name='jellypy_pyCIPAPI',
@@ -16,8 +13,6 @@ setup(
     url='https://github.com/NHS-NGS/JellyPy/pyCIPAPI',
     packages=find_packages(),
     python_requires='>=3.6.*',
-    long_description=README,
-    long_description_content_type="text/markdown",
     install_requires=[
         'docopt == 0.6.2',
         'GelReportModels == 7.2.10',
@@ -26,9 +21,5 @@ setup(
         'requests == 2.22.0',
         'pandas == 0.25.1',
         'openpyxl == 2.6.3'
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3.6"
-    ],
-    include_package_data=True
+    ]
 )
