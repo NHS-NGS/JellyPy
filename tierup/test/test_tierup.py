aeea201f089e5b63805ec918824f1ebfd9eb3ca0
Nana Mensah <Nana.Mensah1@nhs.net>
2020-06-14 16:07:30 +0100

    Add tests for tiering lite class

--- tierup/test/test_tierup.py
+++ tierup/test/test_tierup.py
@@ -1,48 +1,44 @@
+import json
+import os
+from distutils import dir_util
+from pathlib import Path
+
 import pytest
-
+from jellypy.tierup.lib import TieringLite, ReportEvent
 from jellypy.tierup.panelapp import GeLPanel
-from jellypy.tierup.lib import TieringLite
 
-def test_gelpanel_query():
-    # Test gel panel query for ACVRL1 (ENSG00000139567)
-    #   and in Hereditary haemorrhagic telangiectasia (123)
-    acvrl1_ensembl_id = 'ENSG00000139567'
-    gp = GeLPanel(123)
-    results = gp.query(acvrl1_ensembl_id)
-    assert results == (
-        'HGNC:175', 'ACVRL1', '3', 'ENSG00000139567',
-        'MONOALLELIC, autosomal or pseudoautosomal, NOT imprinted'
-    )
-    assert gp.query('NOT_AN_ENSEMBL_ID') == (None, None, None, None, None)
-    assert gp.query('') == (None, None, None, None, None)
 
-TEST_MODE_OF_INHERITANCE = [
-    ("biallelic", "BOTH monoallelic and biallelic, autosomal or pseudoautosomal", True),
-    ("biallelic", "BIALLELIC, autosomal or pseudoautosomal", True),
-    ("mitochondrial", "MITOCHONDRIAL", True),
-    ("DENOVO",  "MONOALLELIC, autosomal or pseudoautosomal, imprinted status unknown", True),
-    ("monoallelic_maternally_imprinted", "MONOALLELIC, autosomal or pseudoautosomal, maternally imprinted (paternal allele expressed)", True),
-    ("xlinked_biallelic", "X-LINKED: hemizygous mutation in males, monoallelic mutations in females may cause disease (may be less severe, later onset than males)", False),
-    ("xlinked_biallelic", "X linked: hemizygous mutation in males, monoallelic mutations in females may cause disease (may be less severe, later onset than males)", False),
-    ("monoallelic", "MONOALLELIC, autosomal or pseudoautosomal", True),
-    ("monoallelic", "Other - please specify in evaluation comments", True),
-    ("monoallelic", "Unknown", True),
-    ("xlinked_biallelic", "X-LINKED: hemizygous mutation in males, biallelic mutations in females", True),
-    ("xlinked_monoallelic", "X-LINKED: hemizygous mutation in males, biallelic mutations in females", True),
-    ("xlinked_monoallelic", "X linked: hemizygous mutation in males, monoallelic mutations in females may cause disease (may be less severe, later onset than males)", True),
-    ("monoallelic_not_imprinted", "MONOALLELIC, autosomal or pseudoautosomal, NOT imprinted", True),
-    ("monoallelic_not_imprinted", "MONOALLELIC, autosomal or pseudoautosomal, imprinted status unknown", True),
-    ("DOMINANT", "", True),
-    (None, "", True),
-    (None, None, True),
-    ("mitochondrial", "BIALLELIC, autosomal or pseudoautosomal", False),
-    ("DENOVO",  "BIALLELIC, autosomal or pseudoautosomal", False),
-    ("biallelic", "MITOCHONDRIAL", False),
-    ("monoallelic_maternally_imprinted", "BIALLELIC, autosomal or pseudoautosomal", False),
-    ("monoallelic", "BIALLELIC, autosomal or pseudoautosomal", False),
-]
+# Read test data from a file
+@pytest.fixture
+def tdata(tmpdir, request):
+    test_dir = Path(request.module.__file__).parent / "test_data"
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmpdir))
+    with open(Path(tmpdir / "test_tierup_data.json")) as f:
+        tdata = json.load(f)
+    return tdata
 
-@pytest.mark.parametrize('tiering,panelapp,result', TEST_MODE_OF_INHERITANCE)
-def test_mode_of_inheritance(tiering, panelapp, result):
+def test_gelpanel_query(tdata):
+    panel_id = tdata["test_gel_panel"][0]["panel_id"]
+    gp = GeLPanel(panel_id)
+    for query, result in tdata["test_gel_panel"][0]["queries"].items():
+        assert gp.query(query) == tuple(result)
+
+def test_mode_of_inheritance(tdata):
     tl = TieringLite()
-    assert tl.moi_match(tiering, panelapp) == result
+    for test_data in tdata["test_mode_of_inheritance"]:
+        tiering, panelapp, result = test_data
+        assert tl._moi_match(tiering, panelapp) == result
+
+def test_high_impact(tdata):
+    tl = TieringLite()
+    for test_data in tdata["test_high_impact"]:
+        segregation, consequence, result = test_data
+        assert tl._is_high_impact(segregation, consequence) == result
+
+def test_tiering_lite(tdata):
+    tl = TieringLite()
+    for td in tdata["test_tiering_lite"]:
+        event = ReportEvent(td['report_event'], td['variant'], td['proband_call'])
+        panel = GeLPanel(td['panel_id'])
+        assert tl.retier(event, panel) == td['result']
