3485becb52f1f45e451240940a5eca1db908b675
Geoff <geoff84woodward@gmail.com>
2020-09-30 15:50:42 +0100

    Update config.py
    anonymised config.py

--- scripts/smsAPI/config.py
+++ scripts/smsAPI/config.py
@@ -1,7 +1,7 @@
-username = '9a8d9d36-0a9a-492f-aa47-adbaa9cd385d'
-password = 'S6a75P7RMvEF4PS01Awo2As9GyYYUqcycYv8UV6IjF8='
+username = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+password = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 auth_url = "https://login.microsoftonline.com/afee026d-8f37-400e-8869-72d9124873e4/oauth2/token"
 sms_url = "https://samplematchservice-gms-beta.genomicsengland.nhs.uk/sample_vcf/"
 
-log_path = "/mnt/K_wgs_logs/"
+log_path = "/path/to/log_stash/"
