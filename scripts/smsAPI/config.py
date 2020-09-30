840db793a74737a0a332bc69fbb1254036c4e971
Geoff <geoff84woodward@gmail.com>
2020-09-30 17:22:19 +0100

    Update config.py
    changed username/password to: client_id/secret

--- scripts/smsAPI/config.py
+++ scripts/smsAPI/config.py
@@ -1,5 +1,5 @@
-username = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-password = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+client_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 auth_url = "https://login.microsoftonline.com/afee026d-8f37-400e-8869-72d9124873e4/oauth2/token"
 sms_url = "https://samplematchservice-gms-beta.genomicsengland.nhs.uk/sample_vcf/"
